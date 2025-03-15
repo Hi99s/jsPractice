ef0b713cb2ab4e43e12e009587b4e793386afea1
WuCan <wucan@haocang.com>
2025-03-15 23:35:02 +0800

    feat: 字母异位词分组

---
+++ js/字母异位词分组.ts
@@ -0,0 +1,26 @@
+const testArr = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+const groupAnagrams = (strs: string[]) => {
+    // 创建一个map作为登记簿
+    const map = new Map<string, string[]>();
+
+    // 遍历每张 "扑克牌"
+    strs.forEach((item) => {
+        // 生成家族标签
+        const sortedStr = item.split("").sort().join("");
+
+        // 如果登记薄里已经有这个家族
+        if (map.has(sortedStr)) {
+            // 加到家族列表中
+            map.get(sortedStr)!.push(item); // 非空断言
+        } else {
+            // 否则新建一个家族，放入第一张牌
+            map.set(sortedStr, [item]);
+        }
+    });
+
+    // 把所有家族列表取出来返回
+    return Array.from(map.values());
+};
+
+console.log(groupAnagrams(testArr));
