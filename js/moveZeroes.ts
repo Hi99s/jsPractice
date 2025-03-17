f10a078fb057ed13dac44fd145358b398550fdb3
Hi99s <2297968568@qq.com>
2025-03-17 23:49:27 +0800

    feat: 移动0

---
+++ js/moveZeroes.ts
@@ -0,0 +1,15 @@
+export {};
+// 双指针
+const nums = [0,1,0,3,12];
+const moveZeroes = (nums: number[]): number[] => {
+    let slow = 0; // 指向非0数的位置
+    for(let fast = 0; fast < nums.length; fast++) {
+        if( nums[fast] !== 0 ) {
+            [nums[slow], nums[fast]] = [nums[fast], nums[slow]]; // 交换位置
+            slow++;
+        }
+    }
+    return nums;
+}
+
+console.log(moveZeroes(nums));
